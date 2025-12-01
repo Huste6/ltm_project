667b4a53711a429af0f67dfc77ba2a2c798d5010
Huste6 <quannghihai1@gmail.com>
2025-12-01 21:32:10 +0700

    ui client

---
+++ client/ui/ui.h
@@ -0,0 +1,62 @@
+#ifndef UI_H
+#define UI_H
+
+#include "../client.h"
+
+/**
+ * @brief Clear screen
+ */
+void ui_clear_screen();
+
+/**
+ * @brief Print banner
+ */
+void ui_print_banner();
+
+/**
+ * @brief Print main menu (not logged in)
+ */
+void ui_print_menu_main();
+
+/**
+ * @brief Print authenticated menu
+ */
+void ui_print_menu_authenticated();
+
+/**
+ * @brief Print room menu
+ */
+void ui_print_menu_room();
+
+/**
+ * @brief Display error message
+ * @param message Error message
+ */
+void ui_show_error(const char *message);
+
+/**
+ * @brief Display success message
+ * @param message Success message
+ */
+void ui_show_success(const char *message);
+
+/**
+ * @brief Display info message
+ * @param message Info message
+ */
+void ui_show_info(const char *message);
+
+/**
+ * @brief Wait for user to press Enter
+ */
+void ui_wait_enter();
+
+/**
+ * @brief Get user input
+ * @param prompt Prompt message
+ * @param buffer Buffer to store input
+ * @param size Buffer size
+ */
+void ui_get_input(const char *prompt, char *buffer, size_t size);
+
+#endif // UI_H
